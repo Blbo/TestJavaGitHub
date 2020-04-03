52040676c2acfef3ff7d8ec98c8b17eb745f43d6
Stephane FREDELLA <stephane.fda@gmail.com>
2020-04-03 20:56:23 +0200

    rename td2 -> td3

--- td3/src/main/java/com/iut/as/StartPoint.java
+++ td3/src/main/java/com/iut/as/StartPoint.java
@@ -9,7 +9,7 @@ public class StartPoint {
 		Mathematic maths = new Mathematic();
 		System.out.println("Le resultat de 2 + 4 est " + maths.addition(2, 4));
 		try {
-			System.out.println("Le resultat de 4 / 2 est " + maths.division(4, 2));
+			System.out.println("Le resultat de 4 / 4 est " + maths.division(4, 4));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
